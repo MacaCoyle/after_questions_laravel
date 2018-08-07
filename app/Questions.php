e54771af6a78fd643bdfba1ad99417ab021b912e
Macarena Coyle <macarena.coyle@avature.net>
2018-08-07 17:42:43 -0300

    df

--- app/Questions.php
+++ app/Questions.php
@@ -4,15 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Preguntas extends Model
+class Questions extends Model
 {
     protected $fillable = [];
     protected $table = "questions";
+
     public function categoria(){
     	return $this->belongsTo(Category::class, "category_id");
     }
-
-    // public function pregunta(){
-    // 	return $this->
-    // }
+    
 }
